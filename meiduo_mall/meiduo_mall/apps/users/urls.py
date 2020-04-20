6bfc888b62afebf221fd7b5a6bdeacbb60e08cad
‘张惠棣’ <1412779356@qq.com>
2020-04-20 16:34:40 +0800

    用户名重复注册后端逻辑

--- meiduo_mall/meiduo_mall/apps/users/urls.py
+++ meiduo_mall/meiduo_mall/apps/users/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path(r'', include('users.urls')),
+    re_path('^usernames/(?P<username>\w{5,20})/count/$', views.UsernameCountView.as_view()),
 ]
--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,3 +1,25 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+from users.models import User
+
+
+class UsernameCountView(View):
+    def get(self, request, username):
+        '''接受用户名，判断该用户名是否可以重复注册'''
+
+        # 1.访问数据库，查看该username 是否有多个，把结果赋给一个变量！
+
+        try:
+            count = User.objects.filter(username=username).count()
+        # 2.如果没有问题，返回json
+        except Exception as e:
+            return JsonResponse({'code': 400,
+                                 'errmsg': '访问数据库失败！'})
+        # 3.如果有问题，返回json！
+        return JsonResponse({'code': 0,
+                             'errmsg': 'ok',
+                             'count': count})
