fd52d0385afc766fca7e6ae08d7914c3c0dc9d69
‘张惠棣’ <1412779356@qq.com>
2020-04-22 10:12:40 +0800

    4.22

---
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -0,0 +1,99 @@
+from django.shortcuts import render
+from django.views import View
+from django_redis import get_redis_connection
+from django.http import HttpResponse
+from meiduo_mall.libs.captcha.captcha import captcha
+
+
+class ImageCodeView(View):
+
+    def get(self, request, uuid):
+        '''生成图形验证码,保存后返回'''
+        # 1.调用captcha框架,生成图片和对应的信息
+        text, image = captcha.generate_captcha()
+
+        # 2.链接redis, 获取redis的链接对象
+        redis_conn = get_redis_connection('verify_code')
+
+        # 3.调用链接对象, 把数据保存到redis
+        redis_conn.setex('img_%s' % uuid, 300, text)
+
+        # 4.返回图片给前端
+        return HttpResponse(image,
+                            content_type='image/jpg')
+
+
+import logging
+
+logger = logging.getLogger('django')
+import random
+from django import http
+from meiduo_mall.libs.yuntongxun.ccp_sms import CCP
+
+
+class SMSCodeView(View):
+
+    def get(self, reqeust, mobile):
+        redis_conn = get_redis_connection('verify_code')
+
+        send_flag = redis_conn.get('send_flag_%s' % mobile)
+        if send_flag:
+            return http.JsonResponse({'code': 400,
+                                      'errmsg': '发送短信过于频繁'})
+        """
+           :param reqeust: 请求对象
+           :param mobile: 手机号
+           :return: JSON
+           """
+        # 1. 接收参数
+        image_code_client = reqeust.GET.get('image_code')
+        uuid = reqeust.GET.get('image_code_id')
+        # all 任意一个为空，则为Fslse
+
+        # 2. 校验参数
+        if not all([image_code_client, uuid]):
+            return http.JsonResponse({'code': 400,
+                                      'errmsg': '缺少必传参数'})
+        # 3. 创建连接到redis的对象
+        image_code_server = redis_conn.get('img_%s' % uuid)
+        # 4. 提取图形验证码 #
+        if image_code_server is None:
+            return http.JsonResponse({'code': 400,
+                                    'errmsg': '图形验证码失效'})
+        # 5. 删除图形验证码，避免恶意测试图形验证码
+        try:
+            redis_conn.delete('img_%s' % uuid)
+        except Exception as e:
+            logger.error(e)
+
+        # 6. 对比图形验证码
+        # bytes 转字符串
+        image_code_server = image_code_server.decode()
+        # 转小写后比较
+        if image_code_client.lower() != image_code_server.lower():
+            # 图形验证码过期或者不存在
+            return http.JsonResponse({'code': 400,
+                                      'errmsg': '输入图形验证码有误'})
+
+        # 7. 生成短信验证码：生成6位数验证码
+        sms_code = '%06d' % random.randint(0, 999999)
+        logger.info(sms_code)
+
+        # 创建管道对象:
+        pl = redis_conn.pipeline()
+        # 8. 保存短信验证码
+        # 短信验证码有效期，单位：300秒
+        # redis_conn.setex('sms_%s' % mobile, 300, sms_code)
+        pl.setex('sms_%s' % mobile, 300, sms_code)
+
+        # redis_conn.setex('send_flag_%s' % mobile, 60, 1)
+        pl.setex('send_flag_%s' % mobile, 60, 1)
+
+        # 执行管道:
+        pl.execute()
+        # 9. 发送短信验证码
+        # 短信模板
+        CCP().send_template_sms(mobile, [sms_code, 5], 1)
+        # 10. 响应结果
+        return http.JsonResponse({'code': 0,
+                                  'errmsg': '发送短信成功'})
